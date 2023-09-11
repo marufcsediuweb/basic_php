f04be550e601d0ea614192b8008811d137719457
arionmaruf <maruf@marufabdullah.com>
2023-09-11 20:17:18 +0600

    first commit

---
+++ personal_info.php
@@ -0,0 +1,9 @@
+<?php
+    $name = "Maruf Abdullah Rion";
+    $age = 30;
+    $country = "Bangladesh";
+    $intro = "I am a computer engineer experienced in Website Design and ClickFunnels, GoHighLevel, and Custom Designing (working remotely since 2013). I do what I love to do and enjoy doing it. I had an interest in Website Designing from a young age.";
+
+    printf("Hey I am %s. I'm %s years old. I live in %s. %s", $name, $age, $country, $intro);
+    
+?>
